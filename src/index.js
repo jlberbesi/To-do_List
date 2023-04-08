3bb4271134f5a7c44a8d24875a25ba8d5cdde3b4
Jlberbesi <jlberbesi.walloom@gmail.com>
2023-04-08 18:57:40 -0500

    Add list strcture

--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const todoList = document.getElementById('todo-list');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  return element;
+class Task {
+  constructor(description, completed = false) {
+    this.description = description;
+    this.completed = completed;
+  }
 }
 
-document.body.appendChild(component());
+const tasks = [
+  new Task('Wash the dishes'),
+  new Task('Do some exercise'),
+  new Task('Write a pull request'),
+];
+
+function renderTasks() {
+  todoList.innerHTML = '';
+
+  tasks.forEach((task, index) => {
+    const taskItem = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = task.completed;
+    checkbox.addEventListener('click', () => {
+      task.completed = checkbox.checked;
+      taskItem.classList.toggle('completed', task.completed);
+    });
+    const description = document.createElement('span');
+    description.textContent = task.description;
+    taskItem.appendChild(checkbox);
+    taskItem.appendChild(description);
+    taskItem.classList.toggle('completed', task.completed);
+    todoList.appendChild(taskItem);
+  });
+}
+
+renderTasks();
